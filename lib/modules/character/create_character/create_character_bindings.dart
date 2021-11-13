513145f9c1a07b6a054faab39b0d96ec6996f7ec
DiegoCastelli <dimarcelo984@gmail.com>
2021-11-13 10:54:57 -0300

    refactoring bindings to use SharedPreferences from Get.it

--- lib/modules/character/create_character/create_character_bindings.dart
+++ lib/modules/character/create_character/create_character_bindings.dart
@@ -3,11 +3,15 @@ import 'package:marvel_test/modules/character/character_controller.dart';
 import 'package:marvel_test/modules/character/create_character/create_character_controller.dart';
 import 'package:marvel_test/repositories/character_repository_implementation.dart';
 import 'package:marvel_test/shared/custom_dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CreateCharacterBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(CreateCharacterController(characterController: Get.find<CharacterController>(), characterRepository: CharacterRepositoryImplementation(http: Get.find<CustomDio>())));
+    Get.put(CreateCharacterController(
+        characterController: Get.find<CharacterController>(),
+        characterRepository: CharacterRepositoryImplementation(
+            prefs: Get.find<SharedPreferences>(),
+            http: Get.find<CustomDio>())));
   }
-
 }
--- lib/modules/character/edit_character/edit_character_bindings.dart
+++ lib/modules/character/edit_character/edit_character_bindings.dart
@@ -3,11 +3,16 @@ import 'package:marvel_test/modules/character/character_controller.dart';
 import 'package:marvel_test/modules/character/edit_character/edit_character_controller.dart';
 import 'package:marvel_test/repositories/character_repository_implementation.dart';
 import 'package:marvel_test/shared/custom_dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class EditCharacterBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(EditCharacterController(character: Get.arguments, characterController: Get.find<CharacterController>(), characterRepository: CharacterRepositoryImplementation(http: Get.find<CustomDio>())));
+    Get.put(EditCharacterController(
+        character: Get.arguments,
+        characterController: Get.find<CharacterController>(),
+        characterRepository: CharacterRepositoryImplementation(
+            prefs: Get.find<SharedPreferences>(),
+            http: Get.find<CustomDio>())));
   }
-
 }
